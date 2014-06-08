7c3796368a4b1f4b6f6bca1bb2dc0a1dba1dcf8d
Rex Suter <rex.suter@gmail.com>
2014-06-08 14:22:09 -0700

    added partial template to render route form on landing page

--- db/migrate/20140608211123_create_vantages.rb
+++ db/migrate/20140608211123_create_vantages.rb
@@ -1,6 +1,10 @@
 class CreateVantages < ActiveRecord::Migration
   def change
     create_table :vantages do |t|
+      t.belongs_to :promenade
+      t.float :latitude
+      t.float :longitude
+      t.timestamps
     end
   end
 end
