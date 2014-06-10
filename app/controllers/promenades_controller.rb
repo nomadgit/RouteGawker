ef5901e0ce9bed429f2fde776fc27f51d64426a9
Rex Suter <rex.suter@gmail.com>
2014-06-09 17:41:40 -0700

    adding partial to root path rendering previous routes saved and redirect to show page for each route on creation

--- app/controllers/promenades_controller.rb
+++ app/controllers/promenades_controller.rb
@@ -1,5 +1,26 @@
 class PromenadesController < ApplicationController
   def index
+    @promenades = Promenade.all()
     @promenade = Promenade.new()
   end
+
+  def create
+    @promenade = Promenade.new(promenade_params)
+    if @promenade.save
+      redirect_to promenade_path(@promenade)
+    else
+      render :index
+    end
+  end
+
+  def show
+    @promenade = Promenade.find(params[:id])
+  end
+
+  private
+
+  def promenade_params
+    params.require(:promenade).permit(:origin_location, :destination_location)
+  end
+
 end
