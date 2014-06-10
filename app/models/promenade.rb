9f5c931cca509390968e26ab03f66950a88935dd
Rex Suter <rex.suter@gmail.com>
2014-06-10 11:03:38 -0700

    added custom before_save to promenade model to properly format address to work with the directions api

--- app/models/promenade.rb
+++ app/models/promenade.rb
@@ -1,3 +1,13 @@
 class Promenade < ActiveRecord::Base
   has_many :vantages
+
+  before_validation :replace_address_spaces_with_plus_signs
+
+  protected
+
+  def replace_address_spaces_with_plus_signs
+    self.origin_location.gsub!(/\s/, '+')
+    self.destination_location.gsub!(/\s/, '+')
+  end
+
 end
