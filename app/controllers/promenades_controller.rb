1d05d4deb81b18ab3d34d6150bbf98394d02ecf5
Rex Suter <rex.suter@gmail.com>
2014-06-11 13:18:17 -0700

    updated show template and controller to actually render the streetview photos from the routes, now need to make it more robust and incorporate the polylines inbetween straight shots

--- app/controllers/promenades_controller.rb
+++ app/controllers/promenades_controller.rb
@@ -1,6 +1,6 @@
-require 'json'
 
 class PromenadesController < ApplicationController
+  require 'json'
   include HTTParty
 
   def index
@@ -19,12 +19,28 @@ class PromenadesController < ApplicationController
 
   def show
     @promenade = Promenade.find(params[:id])
-    response = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{@promenade.origin_location}&destination=#{@promenade.destination_location}&key=#{ENV['API_KEY']}&mode=walking")
-    # print @google_directions_response.body
+    stepwise_collection = get_data(@promenade)
+    stepwise_collection.each do |current_vantage|
+      create_vantage(current_vantage)
+    end
+    @vantages = Vantage.where(promenade_id: params[:id])
+  end
 
-    parsed_object = JSON.parse(response.body)
-    @rex = parsed_object["routes"][0]["legs"][0]
-    puts @rex
+  def get_data(promenade)
+    data = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{@promenade.origin_location}&destination=#{@promenade.destination_location}&key=#{ENV['API_KEY']}&mode=walking").body
+    p "*****" * 300
+    p data
+    p "&&&&&" * 300
+    parsed_dataset = JSON.parse(data)
+    parsed_dataset["routes"][0]["legs"][0]["steps"]
+  end
+
+  def create_vantage(current_vantage)
+    vantage = Vantage.new
+    vantage.latitude = current_vantage["start_location"]["lat"]
+    vantage.longitude = current_vantage["start_location"]["lng"]
+    vantage.promenade_id = params[:id]
+    vantage.save
   end
 
   private
@@ -32,6 +48,7 @@ class PromenadesController < ApplicationController
   def promenade_params
     params.require(:promenade).permit(:origin_location, :destination_location)
   end
+
 
 end
 
